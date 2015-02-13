0198c1fa5616d3669eeeb9e92c54108e7de5dd0e
vicawil <avdl@gmx.ch>
2015-02-13 22:01:31 +0100

    work in progress

--- temp/parseToBioC.rb
+++ temp/parseToBioC.rb
@@ -4,18 +4,38 @@ require 'colorize'
 
 $slice = 30
 $toScreen = false
-$passageCounter = 0
-$debug = true
+
+$passageCounter = 100
+$annotationId = 100
+$relationId = 100
+
+$debug = false
+
+
+def incrRel()
+	$relationId += 1
+end
 
 # Walk statement tree from the root
 def walkStatement(obj, document)
-	puts "[[ #{obj.to_bel} ]]\n".bold
+	if $debug
+		puts "[[ #{obj.to_bel} ]]\n".bold
+	else
+		puts "#{obj.to_bel}\n"
+	end
+
+	# Output of parsed nodes to command-line
 	if $toScreen
 		puts "Subject:".black.on_green.bold + " " + String(obj.subject)
+		# Walk subject term
 		walkTerm(obj.subject, 0)
+		
 		puts "Relationship:".black.on_green.bold + " " + String(obj.relationship)
+		
 		puts "Object:".black.on_green.bold + " " + String(obj.object)
+		# Walk object term
 		walkTerm(obj.object, 0)
+		
 		puts "Annotations: ".light_cyan + String(obj.annotations)
 		puts "Comment: ".light_cyan + String(obj.comment)
 		puts "Subject only?: ".light_cyan + String(obj.subject_only?)
@@ -25,58 +45,64 @@ def walkStatement(obj, document)
 		puts "RDF: ".light_cyan + String(obj.to_rdf).slice(0,$slice)
 		puts "URI: ".light_cyan + String(obj.to_uri)
 		puts "Hash: ".light_cyan + String(obj.hash)
+
+	# Output of parsed nodes to BioC-XML
 	else
 		# Instantiate passage
-		$passageCounter += 1
 		passage = SimpleBioC::Passage.new(document)
 		passage.infons["num"] = $passageCounter
+		$passageCounter += 1
 		passage.offset = 0
 		document.passages << passage
 		
+		unless obj.relationship.nil?
+			
+			# Relationship triple
+			relation = SimpleBioC::Relation.new(document)
+			relation.id = "r" + String($relationId)
+			incrRel()
+			relation.infons["type"] = String(obj.relationship)
+			causeNode = SimpleBioC::Node.new(relation)
+			causeNode.role = "cause"
+			themeNode = SimpleBioC::Node.new(relation)
+			themeNode.role = "theme"
+			triggerNode = SimpleBioC::Node.new(relation)
+			triggerNode.role = "trigger"
+			relation.nodes << causeNode
+			relation.nodes << themeNode
+			relation.nodes << triggerNode
+			document.relations << relation
+			tripleRelation = {"cause" => causeNode, "theme" => themeNode, "trigger" => triggerNode}
+			
+		end
+		
 		# Subject annotation
-		subjAnnotation = SimpleBioC::Annotation.new(document)
-		subjLocation = SimpleBioC::Location.new(subjAnnotation)
-		subjLocation.offset = rand(0..100) # dummy code
-		subjLocation.length = rand(0..100) # dummy code
-		subjAnnotation.locations << subjLocation
-		passage.annotations << subjAnnotation
-		walkTerm(obj.subject, 0, subjAnnotation, :subject)
+		walkTerm(obj.subject, 0, passage, document, tripleRelation, :subject)
 		
 		#http://stackoverflow.com/a/252253
 		unless obj.relationship.nil?
 		
 			# Object annotation
-			objAnnotation = SimpleBioC::Annotation.new(document)
-			objLocation = SimpleBioC::Location.new(objAnnotation)
-			objLocation.offset = rand(0..100) # dummy code
-			objLocation.length = rand(0..100) # dummy code
-			objAnnotation.locations << objLocation
-			passage.annotations << objAnnotation
-			walkTerm(obj.object, 0, objAnnotation, :object)
+			walkTerm(obj.object, 0, passage, document, tripleRelation, :object)
 			
-			# Relation annotation (dummy definition!)
-			relation = SimpleBioC::Relation.new(document)
-			relation.id = rand(0..1000000)# dummy value
-			relation.infons["type"] = String(obj.relationship)
-			causeNode = SimpleBioC::Node.new(relation)
-			themeNode = SimpleBioC::Node.new(relation)
-			triggerNode = SimpleBioC::Node.new(relation)
-			relation.nodes << causeNode
-			relation.nodes << themeNode
-			relation.nodes << triggerNode
-			relation.nodes.each do |currentNode|
-				currentNode.refid = rand(0..1000)
-				currentNode.role = "lorem ipsum dolor sit amet lirum larum loeffelstiel".split(" ")[rand(0..8)]
-			# end dummy definition
-			
-			end
-			document.relations << relation
+			# Relation annotation
+			annotation = SimpleBioC::Annotation.new(document)
+			annotation.id = "a" + String($annotationId)
+			tripleRelation["trigger"].refid = annotation.id
+			$annotationId += 1
+			annotation.infons["trigger"] = obj.relationship
+			annotation.text = nil
+			location = SimpleBioC::Location.new(annotation)
+			location.offset = nil
+			location.length = nil
+			annotation.locations << location
+			passage.annotations << annotation
 		end
 	end
 end
 
 # Recursively walk terms and parameter (leaf nodes)
-def walkTerm(obj, sublevel, annotation = nil, entity = nil)
+def walkTerm(obj, sublevel, passage = nil, document = nil, relation = nil, entity = nil)
 	if $toScreen
 		tab = "\t"*2*sublevel 
 		if obj.instance_of?(BEL::Language::Term)
@@ -98,7 +124,8 @@ def walkTerm(obj, sublevel, annotation = nil, entity = nil)
 			puts tab + "\tArguments: ".yellow + String(obj.arguments)
 			obj.arguments.each do |subobj|
 				if subobj.instance_of?(BEL::Language::Term)
-					puts tab + "\t\tTerm Argument:".yellow
+					puts tab + "\t\tTerm Argument:".yellow + String(subobj)
+					# Recursive call
 					walkTerm(subobj, sublevel + 1)
 				else
 					puts tab + "\t\tParameter Argument: ".green + String(subobj)
@@ -115,14 +142,66 @@ def walkTerm(obj, sublevel, annotation = nil, entity = nil)
 			end
 		end
 	elsif !obj.nil?
+		if sublevel == 0
+			if entity == :subject
+				relation["cause"].refid = "r" + String($relationId)
+			elsif entity == :object
+				relation["theme"].refid = "r" + String($annotationId)
+			end
+		end
+		
 		if obj.instance_of?(BEL::Language::Term)
-			annotation.id = obj.hash # dummy value
-			annotation.infons["type"] = obj.fx.long_form
-			walkTerm(obj.arguments[0], 0, annotation,  entity)
-		else 
+			if !obj.fx.short_form == :proteinAbundance
+				relation = SimpleBioC::Relation.new(document)
+				relation.id = "r" + String($relationId)
+				relation.infons["type"] = obj.fx.long_form
+				relation.infons["BEL (full)"] = String(obj)
+				relation.infons["BEL (relative)"] = String(obj).clone
+				document.relations << relation
+				incrRel()
+				# Recursive call
+				if obj.arguments.length > 1
+					obj.arguments.each do |arg|
+						walkTerm(arg, sublevel + 1, passage, document)
+						node = SimpleBioC::Node.new(relation)
+						node.role = "member"
+						if arg.instance_of?(BEL::Language::Term)
+							node.refid = "r" + String($relationId)
+						elsif arg.instance_of?(BEL::Language::Parameter)
+							node.refid = "a" + String($annotationId)
+						end
+						relation.infons["BEL (relative)"] = relation.infons["BEL (relative)"].sub String(arg), node.refid
+						relation.nodes << node
+					end
+				else
+					arg = obj.arguments[0]
+					walkTerm(arg, sublevel + 1, passage, document)
+					node = SimpleBioC::Node.new(relation)
+					node.role = "self"
+					# Code duplication -> refactoring
+					if arg.instance_of?(BEL::Language::Term)
+						node.refid = "r" + String($relationId)
+					elsif arg.instance_of?(BEL::Language::Parameter)
+						node.refid = "a" + String($annotationId)
+					end
+					relation.infons["BEL (relative)"] = relation.infons["BEL (relative)"].sub String(arg), node.refid
+					relation.nodes << node
+				end
+			else
+				puts "foobar"
+			end
+		else
+			annotation = SimpleBioC::Annotation.new(document)
+			location = SimpleBioC::Location.new(annotation)
+			location.offset = nil
+			location.length = nil
+			annotation.locations << location
+			passage.annotations << annotation
+			annotation.id = "a" + String($annotationId)
+			$annotationId += 1
+			annotation.infons["BEL (full)"] = String(obj)
 			annotation.infons["Entrez GeneID"] = nil # dummy value
 			annotation.infons[obj.ns] = obj.value
-			#location offset?
 			annotation.text = obj.value
 		end
 	end
@@ -134,6 +213,7 @@ end
 ARGV.each do|infile|
 	if !$debug
 		document = SimpleBioC::Document.new(collection)
+		puts "<!--"
 	end
 	belfile = File.new(infile, "r")
 	
@@ -154,10 +234,19 @@ ARGV.each do|infile|
 			document = SimpleBioC::Document.new(collection)
 		end
 		
+		document.id = "d" + String(rand(10000..1000000))
 		walkStatement(obj, document)
 			
 		collection.documents << document
 		
+		if $debug
+			xml = SimpleBioC.to_xml(collection)
+			puts xml
+			puts "\n\n"
+		end
+	end
+	if !$debug
+		puts "-->"
 		xml = SimpleBioC.to_xml(collection)
 		puts xml
 		puts "\n\n"
