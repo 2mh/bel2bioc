0fc3a4aa1dc9fec78a2d33db1f335675edc31aaf
vicawil <avdl@gmx.ch>
2015-02-25 10:26:49 +0100

    ...

---
+++ temp_oo/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ruby bel2bioc.rb -b ../temp_data/set295.bel > ../temp_data/set295.xml
+ruby bel2bioc.rb -b ../temp_data/set268.bel > ../temp_data/set268.xml
+ruby bel2bioc.rb -b ../temp_data/set27.bel > ../temp_data/set27.xml
+
+echo "XML files generated in ../temp_data."
+
