fca3f4fcd71e39b8ba4c3de194e01b7ff17c3898
vicawil <avdl@gmx.ch>
2015-03-08 21:54:32 +0100

    update

--- generate.sh
+++ generate.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo "set295.xml ..."
-ruby bel2bioc.rb -b ../temp_data/set295.bel > ../temp_data/set295.xml
+ruby bel2bioc.rb -b temp_data/set295.bel
 echo "set268.xml ..."
-ruby bel2bioc.rb -b ../temp_data/set268.bel > ../temp_data/set268.xml
+ruby bel2bioc.rb -b temp_data/set268.bel
 echo "set27.xml ..."
-ruby bel2bioc.rb -b ../temp_data/set27.bel > ../temp_data/set27.xml
+ruby bel2bioc.rb -b temp_data/set27.bel
 
-echo "XML files generated in ../temp_data."
+echo "XML files generated in ../temp_data"
 
+echo "Training.tab ..."
+ruby bel2bioc.rb -batmop Task1/Training.tab
+
+echo "XML files generated in ../Task1"
